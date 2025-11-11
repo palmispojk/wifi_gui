778f34aed3feba1480491adb13ef86efd7d8b6e8
Johan Palm <johan.palm2000@gmail.com>
2025-11-11 19:22:52 +0100

    refactoring and removing non used code

--- src/backend/mod.rs
+++ src/backend/mod.rs
@@ -1,19 +1,2 @@
 pub mod error;
 pub mod network_manager;
-
-use crate::backend::error::NMError;
-use async_trait::async_trait;
-
-#[async_trait]
-pub trait WifiBackend {
-    fn scan(&self) -> Result<Vec<WifiNetwork>, NMError>;
-    fn connect(&self, ssid: &str, password: Option<&str>) -> Result<(), NMError>;
-    fn current(&self) -> Result<Option<WifiNetwork>, NMError>;
-}
-
-#[derive(Debug, Clone)]
-pub struct WifiNetwork {
-    pub ssid: String,
-    pub strength: u8,
-    pub secured: bool,
-}
