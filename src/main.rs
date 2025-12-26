c7cfb38bbeb48792cc5d82f7d707634ab2ee945f
Johan Palm <johan.palm2000@gmail.com>
2025-12-26 13:21:36 +0100

    tui expansion into main and short demo

--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,14 @@
 use std::sync::Arc;
-use wifi_gui::backend::{error::NMError, network_manager::NetworkManagerClient};
-use wifi_gui::device::device::DeviceClient;
-use wifi_gui::device::types::DeviceType;
-use wifi_gui::device::wifi::wifi_device::WirelessClient;
+use wifi_gui::tui::TuiManager;
 use zbus::{self, Connection};
 
 #[tokio::main]
-async fn main() -> Result<(), NMError> {
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let conn = Arc::new(Connection::system().await?);
 
-    let nm = NetworkManagerClient::new(conn.clone()).await?;
-
-    let devices = nm.get_device_paths().await?;
-
-    for device_path in devices {
-        let device_client = DeviceClient::new(conn.clone(), device_path).await?;
-
-        if !matches!(device_client.get_device_type().await?, DeviceType::Wifi) {
-            continue;
-        }
-
-        let wifi_client = WirelessClient::new(&device_client).await?;
-        wifi_client.scan().await?;
-
-        let access_points = wifi_client.list_access_points().await?;
-        println!("Found {} access points:", access_points.len());
-
-        for ap in access_points {
-            let ssid = ap.ssid().await.unwrap_or_else(|_| "<hidden>".into());
-            let strength = ap.strength().await.unwrap_or(0);
-            let freq = ap.frequency().await.unwrap_or(0);
-            let security = ap.security().await.unwrap();
-
-            println!(
-                "SSID: {:<25} | Strength: {:>3}% | Freq: {:>4}MHz | Sec: {:?}",
-                ssid, strength, freq, security
-            );
-        }
+    if let Err(e) = TuiManager::run(conn).await {
+        eprintln!("Application error: {}", e);
+        std::process::exit(1);
     }
-
     Ok(())
 }
