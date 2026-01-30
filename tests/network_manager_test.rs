13b5f8fd23c6c266df2d2061423b358e4d77831f
Johan Palm <johan.palm2000@gmail.com>
2026-01-30 16:49:11 +0100

    deleted old tests

--- tests/network_manager_test.rs
+++ tests/network_manager_test.rs
@@ -1,5 +1,4 @@
 use std::sync::Arc;
-use wifi_gui::backend::error::NMError;
 use wifi_gui::backend::network_manager::NetworkManagerClient;
 
 #[tokio::test]
@@ -13,32 +12,4 @@ async fn test_create_nm_client() {
     let client = NetworkManagerClient::new(conn_arc.clone()).await;
 
     assert!(client.is_ok(), "Failed to create NetworkManagerClient");
-}
-
-#[tokio::test]
-async fn test_get_device_paths() {
-    let conn = zbus::Connection::system()
-        .await
-        .expect("System bus expected!");
-
-    let conn_arc = Arc::new(conn);
-
-    let client = NetworkManagerClient::new(conn_arc.clone()).await.unwrap();
-
-    match client.get_device_paths().await {
-        Ok(devices) => {
-            assert!(
-                !devices.is_empty(),
-                "Expected at least one device, got none"
-            );
-            println!("Devices:");
-            for d in devices {
-                println!("  {}", d);
-            }
-        }
-        Err(NMError::NoDeviceFound) => {
-            panic!("No devices found, but system probably has Wi-Fi or Ethernet")
-        }
-        Err(err) => panic!("Unexpected error: {:?}", err),
-    }
 }
