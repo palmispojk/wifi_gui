cf00dc7a259106e209cc4f4d9eb88dde2ae71f0a
Johan Palm <johan.palm2000@gmail.com>
2026-01-30 16:47:53 +0100

    features for rendering and connecting to access points

--- src/backend/network_manager.rs
+++ src/backend/network_manager.rs
@@ -28,12 +28,18 @@ pub trait NetworkManager {
     //
     async fn get_all_devices(&self) -> Result<Vec<OwnedObjectPath>, NMError>;
 
-    async fn add_and_activate_connection(
+    fn add_and_activate_connection(
         &self,
-        connection: Value<'_>,
-        device: &OwnedObjectPath,
-        specific_object: &OwnedObjectPath,
-    ) -> Result<(OwnedObjectPath, OwnedObjectPath), NMError>;
+        connection: std::collections::HashMap<
+            &str,
+            std::collections::HashMap<&str, zbus::zvariant::Value<'_>>,
+        >,
+        device: &zbus::zvariant::ObjectPath<'_>,
+        specific_object: &zbus::zvariant::ObjectPath<'_>,
+    ) -> zbus::Result<(
+        zbus::zvariant::OwnedObjectPath,
+        zbus::zvariant::OwnedObjectPath,
+    )>;
 }
 
 /// Client for interacting with the NetworkManager service.
@@ -42,10 +48,13 @@ pub trait NetworkManager {
 pub struct NetworkManagerClient {
     conn: Arc<Connection>,
     proxy: NetworkManagerProxy<'static>,
+    wifi_device_client: Option<DeviceClient>,
 }
 
 impl NetworkManagerClient {
     /// Creates a new `NetworkManagerClient` using the given D-Bus connection.
+    ///
+    /// *Note*: `wifi_device_client` is `None` if there is no hardware
     ///
     /// # Arguments
     ///
@@ -57,7 +66,15 @@ impl NetworkManagerClient {
     /// the proxy cannot be built.
     pub async fn new(conn: Arc<Connection>) -> Result<Self, NMError> {
         let proxy = NetworkManagerProxy::new(&conn).await?;
-        Ok(Self { conn, proxy })
+
+        let mut client = Self {
+            conn,
+            proxy,
+            wifi_device_client: None,
+        };
+
+        client.wifi_device_client = client.get_device_of_type(DeviceType::Wifi).await.ok();
+        Ok(client)
     }
 
     /// Returns the paths of all active devices.
@@ -65,7 +82,7 @@ impl NetworkManagerClient {
     /// # Errors
     ///
     /// Returns `NMError::NoDeviceFound` if no devices are available.
-    pub async fn get_device_paths(&self) -> Result<Vec<OwnedObjectPath>, NMError> {
+    async fn get_device_paths(&self) -> Result<Vec<OwnedObjectPath>, NMError> {
         let devices = self.proxy.get_devices().await?;
         if devices.is_empty() {
             return Err(NMError::NoDeviceFound);
@@ -73,43 +90,56 @@ impl NetworkManagerClient {
         Ok(devices)
     }
 
+    /// Finds the first device matching the specified [`DeviceType`].
+    ///
+    /// # Errors
+    /// Returns [`NMError::NoDeviceFound`] if no hardware of that type is detected.
+    async fn get_device_of_type(&self, device_type: DeviceType) -> Result<DeviceClient, NMError> {
+        let dev_paths = self.get_device_paths().await?;
+
+        for path in dev_paths {
+            let device = DeviceClient::new(self.conn.clone(), path).await?;
+
+            if device.get_device_type().await? == device_type {
+                return Ok(device);
+            }
+        }
+        Err(NMError::NoDeviceFound)
+    }
+
     /// Performs a full scan of all available Wi-Fi access points across all devices.
     ///
-    /// This function iterates through all network devices, filters for Wi-Fi hardware,
-    /// triggers a scan, and deduplicates the results by SSID to return only the
+    /// Triggers a scan, and deduplicates the results by SSID to return only the
     /// strongest signal for each network.
     ///
     /// # Errors
-    /// Returns an [`NMError`] if the D-Bus connection fails or if NetworkManager
-    /// is unreachable.
+    /// Returns an [`NMError`] if the D-Bus connection fails, if NetworkManager
+    /// is unreachable or there is no Wifi hardware.
     pub async fn scan_all_wifi_networks(&self) -> Result<Vec<NetworkDisplayInfo>, NMError> {
         let mut best_networks: HashMap<String, NetworkDisplayInfo> = HashMap::new();
-        let paths = self.get_device_paths().await?;
 
-        for path in paths {
-            let dev = DeviceClient::new(self.conn.clone(), path).await?;
+        let wifi = match &self.wifi_device_client {
+            Some(dev) => WirelessClient::new(dev).await?,
+            None => return Err(NMError::NoDeviceFound),
+        };
 
-            if matches!(dev.get_device_type().await?, DeviceType::Wifi) {
-                let wifi = WirelessClient::new(&dev).await?;
+        let _ = wifi.scan().await?;
 
-                let _ = wifi.scan().await?;
+        let aps = wifi.list_access_points().await?;
+        for ap in aps {
+            let info = NetworkDisplayInfo::new(&ap).await?;
+            let ssid = info.ssid.clone();
 
-                let aps = wifi.list_access_points().await?;
-                for ap in aps {
-                    let info = NetworkDisplayInfo::new(&ap).await?;
-                    let ssid = info.ssid.clone();
-
-                    best_networks
-                        .entry(ssid)
-                        .and_modify(|existing| {
-                            if info.strength > existing.strength {
-                                *existing = info.clone();
-                            }
-                        })
-                        .or_insert(info);
-                }
-            }
+            best_networks
+                .entry(ssid)
+                .and_modify(|existing| {
+                    if info.strength > existing.strength {
+                        *existing = info.clone();
+                    }
+                })
+                .or_insert(info);
         }
+
         let mut results: Vec<NetworkDisplayInfo> = best_networks.into_values().collect();
         results.sort_by(|a, b| b.strength.cmp(&a.strength));
         Ok(results)
@@ -118,7 +148,6 @@ impl NetworkManagerClient {
     /// Connects to a wifi accesspoint
     ///
     /// # Arguments
-    /// `device_path` - A reference to the objectpath of the device
     /// `ap_path` - A reference path to the accesspoint
     /// `security` - What security the accesspoint
     /// `password` - An optional password if there is no password needed
@@ -127,18 +156,24 @@ impl NetworkManagerClient {
     /// `NMError` - for any error with dbus with the proxy or connection
     pub async fn connect_to_wifi(
         &self,
-        device_path: &OwnedObjectPath,
+        ssid: &str,
         ap_path: &OwnedObjectPath,
         security: WifiSecurity,
         password: Option<&str>,
-    ) -> Result<(), NMError> {
-        let connection_dict = Value::from(security.to_nm_dict(password));
+    ) -> Result<String, NMError> {
+        let device_path = self
+            .wifi_device_client
+            .as_ref()
+            .ok_or(NMError::NoDeviceFound)?
+            .path();
+
+        let connection_settings = security.build_connection_settings(ssid.as_bytes(), password);
 
         self.proxy
-            .add_and_activate_connection(connection_dict, device_path, ap_path)
+            .add_and_activate_connection(connection_settings, device_path, ap_path)
             .await?;
 
-        Ok(())
+        Ok(format!("Connected to Access Point {}", ap_path.to_string()))
     }
 
     /// Starts a background task to listen for D-Bus signals from NetworkManager.
